51e382bc4852be67d122cfb3b80bbbe6cda137a4
matheus0liveira <matheusfilho98.ms@gmailcom>
2023-02-04 12:48:01 -0300

    refactor: improve socket store

--- src/stores/socket.store.ts
+++ src/stores/socket.store.ts
@@ -1,13 +1,30 @@
-import { Socket } from 'socket.io-client';
+import { connect, Socket } from 'socket.io-client';
 import { create } from 'zustand';
 
 export type SocketStore = {
   socket?: Socket;
+
+  /**
+   * @deprecated
+   */
   changeSocket: (s: Socket) => void;
+  setSocket: () => void;
 };
 
 export const useSocketStore = create<SocketStore>((set) => ({
   changeSocket(socket) {
+    set((s) => ({ ...s, socket }));
+  },
+  setSocket() {
+    const socket = connect('http://localhost:3000', {
+      path: '/api/socketio',
+    });
+
+    // log socket connection
+    socket.on('connect', () => {
+      console.log('SOCKET CONNECTED!', socket.id);
+    });
+
     set((s) => ({ ...s, socket }));
   },
 }));
